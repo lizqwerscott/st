6d9ebd9818d0007f8a009e84cf378c06a7796903
lizqwerscott <lizqwerscott@163.com>
2022-09-10 14:18:47 +0800

    change font

--- config.h
+++ config.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 //Source Code Pro:
-static char *font = "Source Code Pro:pixelsize=18:antialias=true:autohint=true";
+static char *font = "Source Code Pro:pixelsize=20:antialias=true:autohint=true";
 static int borderpx = 4;
 
 /*
